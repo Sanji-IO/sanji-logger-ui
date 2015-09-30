d0ac29a948b31922ac10c97c1bf272e6251383fe
zack9433 <zack9433@gmail.com>
2015-09-30 14:20:10 +0800

    Add coverall

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,8 @@ module.exports = function(config) {
       'karma-sourcemap-loader',
       'karma-webpack',
       'karma-coverage',
-      'karma-mocha-reporter'
+      'karma-mocha-reporter',
+      'karma-coveralls'
     ],
 
     webpack: webpackConfig,
@@ -44,7 +45,13 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha', 'coverage'],
+    reporters: ['mocha', 'coverage', 'coveralls'],
+
+
+    coverageReporter: {
+      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      dir: 'coverage/'
+    },
 
 
     // Mocha reporter options
