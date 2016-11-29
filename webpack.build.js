10f54350c95fbef09c70c76871500442c8db4706
Zack Yang <zack9433@gmail.com>
2016-11-29 22:58:55 +0800

    refactor(webpack): update webpack config

--- webpack.build.js
+++ webpack.build.js
@@ -17,12 +17,12 @@ config.externals = [
 ];
 
 config.module.rules = [
-  {test: /\.js$/, loader: 'ng-annotate', exclude: /(node_modules)/, enforce: 'post'},
+  {test: /\.js$/, loader: 'ng-annotate-loader', exclude: /(node_modules)/, enforce: 'post'},
   {
     test: /\.scss$/,
     loader: ExtractTextPlugin.extract({
       notExtractLoader: 'style-loader',
-      loader: 'css!postcss!sass?includePaths[]=' + bourbon
+      loader: 'css-loader!postcss-loader!sass-loader?includePaths[]=' + bourbon
     })
   },
   {test: /\.css$/, loader: 'style!css!postcss'}
@@ -30,7 +30,6 @@ config.module.rules = [
 
 config.plugins.push(
   new ExtractTextPlugin('sanji-logger-ui.css'),
-  new webpack.optimize.DedupePlugin(),
   new webpack.LoaderOptionsPlugin({
     minimize: true,
     debug: false,
